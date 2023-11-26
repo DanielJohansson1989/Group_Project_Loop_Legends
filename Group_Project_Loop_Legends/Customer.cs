ad37ce2494f2b49c3446942925ca9717da24ef0a
John Praesto <john_praesto@hotmail.com>
2023-11-26 18:10:14 +0100

    Added lists of customers and admins to be used in the login logic. I've made a login menu, but not any login functionality. / JP

--- Group_Project_Loop_Legends/Customer.cs
+++ Group_Project_Loop_Legends/Customer.cs
@@ -11,10 +11,9 @@ namespace Group_Project_Loop_Legends
     {
         private List<Account> _accountList = new List<Account>();
         private List<string> _historyList = new List<string>();
-        private double _credit;
-        public Customer(string _name, string _password, int _logInTries, double credit) : base(_name, _password, _logInTries)
+        private double _credit = 0;
+        public Customer(string _name, string _password, int _logInTries) : base(_name, _password, _logInTries)
         {
-            this._credit = credit;
         }
         public override void Menu()
         {
--- Group_Project_Loop_Legends/Program.cs
+++ Group_Project_Loop_Legends/Program.cs
@@ -5,11 +5,17 @@
         static void Main(string[] args)
         {
             // Creating base users and accounts for initial use and testing
-            Customer c1 = new Customer("Anton", "password", 0, 0);
-            Customer c2 = new Customer("Daniel", "password", 0, 0);
-            Customer c3 = new Customer("John", "password", 0, 0);
-            Customer c4 = new Customer("Rasmus", "password", 0, 0);
-            Admin a1 = new Admin("Pär", "password", 0);
+            Customer c1 = new Customer("Anton", "passwordA", 0);
+            Customer c2 = new Customer("Daniel", "passwordD", 0);
+            Customer c3 = new Customer("John", "passwordJ", 0);
+            Customer c4 = new Customer("Rasmus", "passwordR", 0);
+
+            List<Customer> customerList = new List<Customer>() { c1, c2, c3, c4};
+
+            Admin a1 = new Admin("Pär", "passwordP", 0);
+            Admin a2 = new Admin("Tobias", "passwordT", 0);
+
+            List<Admin> adminList = new List<Admin> { a1, a2 };
 
             Account c1a1 = new Account(3000, "SEK", "Antons Lönekonto", "Anton");
             Account c1a2 = new Account(50000, "USD", "Antons Sparkonto", "Anton");
@@ -17,7 +23,7 @@
             c1.CreateNewAccount(c1a1);
             c1.CreateNewAccount(c1a2);
 
-            c1.SeeAccounts();
+            User.LogIn(customerList, adminList);
 
             Console.ReadLine();
         }
