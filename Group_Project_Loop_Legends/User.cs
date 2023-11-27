ca9fa1115e533665327b6f9ea8a6ec37e1d35da4
Rasmus Glasell Johansson <ragl025@gmail.com>
2023-11-27 09:40:01 +0100

    moved user login metod from user

--- Group_Project_Loop_Legends/User.cs
+++ Group_Project_Loop_Legends/User.cs
@@ -19,47 +19,7 @@ namespace Group_Project_Loop_Legends
             this._password = password;
             this._logInTries = logInTries;
         }
-        public static void LogIn(List<Customer> customerList, List<Admin> adminList) // Make LogIn its own class instead of method in User? S in SOLID principles
-        {
-            Console.Clear();
-            Console.WriteLine("Welcome to the Loop Legends Bank\n");
-            Console.WriteLine("What would you like to log in as? (user arrow keys to navigate, then press Enter)");
-            Console.WriteLine("\n     Admin\n     Customer");
-            int y = 4;
-            Console.SetCursorPosition(0, y);
-            Console.CursorVisible = false;
-            Console.Write("-->");
-            ConsoleKeyInfo navigator;
-            navigator = Console.ReadKey();
-            while (navigator.Key != ConsoleKey.Enter)
-            {
-                navigator = Console.ReadKey();
-                Console.SetCursorPosition(0, y);
-                Console.Write("   ");
-                if (navigator.Key == ConsoleKey.UpArrow && y > 4)
-                {
-                    y--;
-                }
-                else if (navigator.Key == ConsoleKey.DownArrow && y < 5)
-                {
-                    y++;
-                }
-                Console.SetCursorPosition(0, y);
-                Console.Write("-->");
-            }
-
-            Console.Clear();
-            Console.CursorVisible = true;
-
-            if (y == 4) // Log in Admin
-            {
-
-            }
-            else if(y == 5) // Log in Customer
-            {
-                
-            }
-        }
+       
 
         public virtual void Menu()
         {
