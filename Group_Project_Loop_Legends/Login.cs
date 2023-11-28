818f03d9ac8aec04b759b98af7405a79a7c1d0d2
John Praesto <john_praesto@hotmail.com>
2023-11-28 09:20:37 +0100

    Moved initial test users and lists from Main to LogIn class / JP

--- Group_Project_Loop_Legends/Login.cs
+++ Group_Project_Loop_Legends/Login.cs
@@ -8,8 +8,29 @@ namespace Group_Project_Loop_Legends
 {
     internal class Login
     {
-        public static void LogIn(List<Customer> customerList, List<Admin> adminList) // Make LogIn its own class instead of method in User? S in SOLID principles
+        public static void LogIn() // Make LogIn its own class instead of method in User? S in SOLID principles
         {
+            ///////////////////////////////////////////////
+            // Creating base users and accounts for initial use and testing
+            Customer c1 = new Customer("Anton", "passwordA", 0);
+            Customer c2 = new Customer("Daniel", "passwordD", 0);
+            Customer c3 = new Customer("John", "passwordJ", 0);
+            Customer c4 = new Customer("Rasmus", "passwordR", 0);
+
+            List<Customer> customerList = new List<Customer>() { c1, c2, c3, c4 };
+
+            Admin a1 = new Admin("Pär", "passwordP", 0);
+            Admin a2 = new Admin("Tobias", "passwordT", 0);
+
+            List<Admin> adminList = new List<Admin> { a1, a2 };
+
+            Account c1a1 = new Account(3000, "SEK", "Antons Lönekonto", "Anton");
+            Account c1a2 = new Account(50000, "USD", "Antons Sparkonto", "Anton");
+
+            c1.CreateNewAccount(c1a1);
+            c1.CreateNewAccount(c1a2);
+            //////////////////////////////////////////////////7
+
             Console.Clear();
             Console.WriteLine("Welcome to the Loop Legends Bank\n");
             Console.WriteLine("What would you like to log in as? (user arrow keys to navigate, then press Enter)");
--- Group_Project_Loop_Legends/Program.cs
+++ Group_Project_Loop_Legends/Program.cs
@@ -4,28 +4,7 @@
     {
         static void Main(string[] args)
         {
-            // Creating base users and accounts for initial use and testing
-            Customer c1 = new Customer("Anton", "passwordA", 0);
-            Customer c2 = new Customer("Daniel", "passwordD", 0);
-            Customer c3 = new Customer("John", "passwordJ", 0);
-            Customer c4 = new Customer("Rasmus", "passwordR", 0);
-
-            List<Customer> customerList = new List<Customer>() { c1, c2, c3, c4};
-
-            Admin a1 = new Admin("Pär", "passwordP", 0);
-            Admin a2 = new Admin("Tobias", "passwordT", 0);
-
-            List<Admin> adminList = new List<Admin> { a1, a2 };
-
-            Account c1a1 = new Account(3000, "SEK", "Antons Lönekonto", "Anton");
-            Account c1a2 = new Account(50000, "USD", "Antons Sparkonto", "Anton");
-
-            c1.CreateNewAccount(c1a1);
-            c1.CreateNewAccount(c1a2);
-
-            Login.LogIn(customerList, adminList);
-
-            Console.ReadLine();
+            Login.LogIn();
         }
     }
 }
