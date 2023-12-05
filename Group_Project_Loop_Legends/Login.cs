254a3ecada097c824b55be33c53c7adf6fd39507
Rasmus Glasell Johansson <ragl025@gmail.com>
2023-12-05 09:43:00 +0100

    Lockout after 3 failed login attempts + error message

--- Group_Project_Loop_Legends/Login.cs
+++ Group_Project_Loop_Legends/Login.cs
@@ -8,6 +8,7 @@ namespace Group_Project_Loop_Legends
 {
     internal class Login
     {
+        static int TriesLeft = 3;
         public static void LogIn(List<Customer> customerList, List<Admin> adminList)
         {
             Console.Clear();
@@ -19,11 +20,18 @@ namespace Group_Project_Loop_Legends
             Console.WriteLine("Welcome to the Loop Legends Bank\n");
             Console.WriteLine("What would you like to log in as? (user arrow keys to navigate, then press Enter)");
             Console.WriteLine("\n     Admin\n     Customer");
-            int y = 9;
+            if (TriesLeft == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n\nNo more login tries left. . .");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            
+            int y = 9;           
             Console.SetCursorPosition(0, y);
             Console.CursorVisible = false;
             Console.Write("-->");
-            ConsoleKeyInfo navigator;
+            ConsoleKeyInfo navigator;            
             do
             {
                 navigator = Console.ReadKey();
@@ -44,9 +52,9 @@ namespace Group_Project_Loop_Legends
             Console.Clear();
             Console.CursorVisible = true;
 
-            if (y == 9) // Log in Admin
+            if (y == 9 && TriesLeft != 0) // Log in Admin
             {
-                for (int i = 3; i > 0;)
+                for (TriesLeft = 3; TriesLeft > 0;)
                 {
                     Console.Clear();
                     Console.Write("Username: ");
@@ -58,22 +66,27 @@ namespace Group_Project_Loop_Legends
                     
                     if (uCheck == null || uCheck._password != password)
                     {
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Invalid username or password. Please try again.");
-                        i--;
-                        Console.WriteLine($"{i} tries left");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        TriesLeft--;
+                        Console.WriteLine($"{TriesLeft} tries left");
                         Console.ReadKey();
                     }
                     else
                     {
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Login successful . . .");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        TriesLeft = 3;
                         uCheck.Menu();
                         break;
                     }                                       
                 }
             }
-            else if (y == 10) // Log in Customer
+            else if (y == 10 && TriesLeft != 0) // Log in Customer
             {
-                for (int i = 3; i > 0;)
+                for (TriesLeft = 3; TriesLeft > 0;)
                 {
                     Console.Clear();
                     Console.Write("Username: ");
@@ -85,14 +98,19 @@ namespace Group_Project_Loop_Legends
 
                     if (uCheck == null || uCheck._password != password)
                     {
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Invalid username or password. Please try again.");
-                        i--;
-                        Console.WriteLine($"{i} tries left");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        TriesLeft--;
+                        Console.WriteLine($"{TriesLeft} tries left");
                         Console.ReadKey();
                     }
                     else
                     {
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Login successful . . .");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        TriesLeft = 3;
                         uCheck.Menu(customerList);
                         break;
                     }
